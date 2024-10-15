4daf0e3bc6efb595828e3ed96a2a6c9d514cf417
dmcvicar <mlops.solutions@gmail.com>
2024-10-15 08:35:15 -0600

    removed unused config from nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.resolve.fallback = {
-
-      // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped.
-      ...config.resolve.fallback,  
-
-      fs: false, // the solution
-    };
-    
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
